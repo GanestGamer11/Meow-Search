0ef84bf31661a86cc3ed6b000376c856e9eabe63
ganestgamer11 <ganestgamer11@gmail.com>
2023-05-28 10:27:43 -0300

    finalizando a pagina result e ajustes modo noturno

--- darkButton.js
+++ darkButton.js
@@ -2,20 +2,22 @@ document.body.style="background-color: var(--bs-dark);transition: 0.5s;"
 const sun = "https://www.uplooder.net/img/image/55/7aa9993fc291bc170abea048589896cf/sun.svg";
 const moon = "https://www.uplooder.net/img/image/2/addf703a24a12d030968858e0879b11e/moon.svg"
 
-var theme = "dark";
+var theme = "light";
   const root = document.querySelector(":root");
   const container = document.getElementsByClassName("theme-container")[0];
   const themeIcon = document.getElementById("theme-icon");
   container.addEventListener("click", setTheme);
   function setTheme() {
     switch (theme) {
-      case "dark":
-        setLight();
-        theme = "light";
-        break;
       case "light":
+        console.log("aqui1");
         setDark();
         theme = "dark";
+        break;
+      case "dark":
+        console.log("aqui");
+        setLight();
+        theme = "light";
         break;
     }
   }
